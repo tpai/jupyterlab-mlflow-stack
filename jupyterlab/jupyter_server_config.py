350323388a1fe79a159cefe26beb90ad3615cbc5
Tony Pai <tony77794@gmail.com>
2025-02-08 14:01:12 +0800

    Add default password for jupyterlab

---
+++ jupyterlab/jupyter_server_config.py
@@ -0,0 +1,3 @@
+# >>> from jupyter_server.auth import passwd
+# >>> passwd('jupyterpass')
+c.ServerApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$vhtBJMdz+bJ/Qu+J41MRSA$JHFpk462st+rEsjSERyTPp/vjJbWX5cJQr1hlUKpkHc'
